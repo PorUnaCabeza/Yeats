0582db74ac9d5bf288058202fcbc74636cfff484
PorUnaCabeza <makpia@163.com>
2016-09-04 23:10:37 +0800

    微博验证码登录

--- src/main/java/util/AccountPool.java
+++ src/main/java/util/AccountPool.java
@@ -1,13 +1,19 @@
 package util;
 
+import com.github.bingoohuang.utils.codec.*;
 import dao.AccountDao;
 import entity.Account;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 import org.jsoup.Connection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -19,6 +25,8 @@ public class AccountPool {
     private static Logger log = LoggerFactory.getLogger(AccountPool.class);
 
     private static List<Account> accountList;
+
+    private static Scanner sc = new Scanner(System.in);
 
     static {
         accountList = AccountDao.getAccountList();
@@ -39,7 +47,7 @@ public class AccountPool {
         try {
             Connection.Response rs = con.cookies(account.getCookies()).execute();
             if (rs.body().indexOf("\"isLogin\":true") > 0) {
-                log.info(account.getUserName() + "登录成功,状态码" + rs.statusCode());
+                log.info(account.getUserName() + "使用保存的cookie登录成功,状态码" + rs.statusCode());
                 account.setLogin(true);
             }
         } catch (IOException e) {
@@ -49,7 +57,6 @@ public class AccountPool {
     }
 
     private static boolean loginBySavedCookies(Account account) {
-        Map<String, String> cookies = null;
         if (account.getCookies() == null) {
             log.info("读取的cookie为空");
             return false;
@@ -58,6 +65,15 @@ public class AccountPool {
     }
 
     private static boolean loginByPassword(Account account) {
+        log.info(account.getUserName() + "进行账号密码登录...");
+        Boolean needCaptcha = checkCaptcha(account);
+        String pcid = null;
+        String captchaContent = null;
+        if (needCaptcha) {
+            pcid = getCaptcha();
+            System.out.println(account.getUserName() + "登录需要验证码,请输入验证码");
+            captchaContent = sc.nextLine();
+        }
         Connection con = JsoupUtil.getPostCon(Config.getValue("loginUrl"));
         Connection.Response rs = null;
         try {
@@ -66,17 +82,21 @@ public class AccountPool {
             con.data("savestate", "1");
             con.data("ec", "0");
             con.data("entry", "mweibo");
+            if (needCaptcha) {
+                con.data("pincode", captchaContent);
+                con.data("pcid", pcid);
+            }
             con.referrer("https://passport.weibo.cn/signin/login");
             rs = con.execute();
             JSONObject jsonObject = new JSONObject(rs.body());
             String retCode = jsonObject.get("retcode").toString();
             if (retCode.equals("20000000")) {
-                log.info("登录成功");
+                log.info(account.getUserName() + "帐号密码登录成功");
                 account.setLogin(true);
                 account.setCookies(rs.cookies());
                 AccountDao.updateAccount(account);
             } else {
-                log.info("登录失败");
+                log.info(account.getUserName() + "账号密码登录失败");
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -99,5 +119,61 @@ public class AccountPool {
         }
     }
 
+    private static boolean checkCaptcha(Account account) {
+        String base64Str = null;
+        try {
+            base64Str = com.github.bingoohuang.utils.codec.Base64.base64(URLEncoder.encode(account.getUserName(), "utf8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        Connection con = JsoupUtil.getGetCon(
+                Config.getValue("preLogin")
+                        .replaceAll("#userBase64#", base64Str)
+                        .replaceAll("#unixTime#", System.currentTimeMillis() + "")
+        ).ignoreContentType(true);
+        Connection.Response rs = null;
+        try {
+            rs = con.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject preLoginJson = new JSONObject(rs.body().replaceAll("jsonpcallback\\d+\\((\\{.*?\\})\\)", "$1"));
+        if (preLoginJson.getInt("showpin") == 0)
+            return false;
+        return true;
+    }
+
+    private static String getCaptcha() {
+        Connection captchaCon = JsoupUtil.getGetCon(Config.getValue("captchaImage")).ignoreContentType(true);
+        Connection.Response captchaResponse = null;
+        try {
+            captchaResponse = captchaCon.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject captchaDataJson = new JSONObject(captchaResponse.body()).getJSONObject("data");
+        byte[] bytes = com.github.bingoohuang.utils.codec.Base64.unBase64(StringUtils.substringAfter(captchaDataJson.getString("image"), "base64,"));
+        String pcid = captchaDataJson.getString("pcid");
+        File captchaFile = new File("yeats_captcha.png");
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(captchaFile);
+            out.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("验证码已保存" + ",路径为:" + captchaFile.getAbsolutePath() + "  pcid:" + pcid);
+        return pcid;
+    }
+
+    public static void main(String[] args) {
+        new AccountPool();
+    }
 
 }
--- src/test/java/CaptchaTest.java
+++ src/test/java/CaptchaTest.java
@@ -1,12 +1,16 @@
+import com.github.bingoohuang.utils.codec.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONObject;
 import org.jsoup.Connection;
 import org.junit.Test;
-import util.Base64;
+import util.AccountPool;
 import util.Config;
 import util.JsoupUtil;
+import util.YeatsUtil;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URLEncoder;
+import java.util.Scanner;
 
 /**
  * Created by Cabeza on 2016-09-01.
@@ -16,7 +20,7 @@ public class CaptchaTest {
     public void makeSuTest() {
         String su = "17079715071";
         try {
-            su = Base64.encode(URLEncoder.encode(su, "utf8"));
+            su = Base64.base64(URLEncoder.encode(su, "utf8"));
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
@@ -33,9 +37,10 @@ public class CaptchaTest {
     @Test
     public void preLoginTest() {
         String userName = "17079715071";
+        String password = "11";
         String base64Str = null;
         try {
-            base64Str = Base64.encode(URLEncoder.encode(userName, "utf8"));
+            base64Str = com.github.bingoohuang.utils.codec.Base64.base64(URLEncoder.encode(userName, "utf8"));
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
@@ -50,6 +55,57 @@ public class CaptchaTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(rs.body());
+        JSONObject preLoginJson = new JSONObject(rs.body().replaceAll("jsonpcallback\\d+\\((\\{.*?\\})\\)", "$1"));
+        if (preLoginJson.getInt("showpin") == 0) return;
+        Connection captchaCon = JsoupUtil.getGetCon(Config.getValue("captchaImage")).ignoreContentType(true);
+        Connection.Response captchaResponse = null;
+        try {
+            captchaResponse = captchaCon.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject captchaDataJson = new JSONObject(captchaResponse.body()).getJSONObject("data");
+        byte[] bytes = com.github.bingoohuang.utils.codec.Base64.unBase64(StringUtils.substringAfter(captchaDataJson.getString("image"), "base64,"));
+        String pcid = captchaDataJson.getString("pcid");
+        File captchaFile = new File("yeats_captcha.png");
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(captchaFile);
+            out.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("验证码已保存" + ",路径为:" + captchaFile.getAbsolutePath() + "  pcid:" + pcid);
+        Scanner sc = new Scanner(System.in);
+        String captchaContent = sc.nextLine();
+        Connection loginCon = JsoupUtil.getPostCon(Config.getValue("loginUrl"));
+        Connection.Response loginResponse = null;
+        try {
+            loginCon.data("username", userName);
+            loginCon.data("password", password);
+            loginCon.data("savestate", "1");
+            loginCon.data("ec", "0");
+            loginCon.data("entry", "mweibo");
+            loginCon.data("pincode", captchaContent);
+            loginCon.data("pcid", pcid);
+            loginCon.referrer("https://passport.weibo.cn/signin/login");
+            loginResponse = loginCon.execute();
+            JSONObject jsonObject = new JSONObject(loginResponse.body());
+            String retCode = jsonObject.get("retcode").toString();
+            if (retCode.equals("20000000")) {
+                System.out.println("success");
+            } else {
+                System.out.println("failed");
+            }
+            System.out.println(YeatsUtil.unicode2Character(loginResponse.body()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
