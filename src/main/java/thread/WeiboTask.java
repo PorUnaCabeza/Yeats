6c142a46f1da6fee81b0994f36580e53914b06ea
PorUnaCabeza <makpia@163.com>
2016-09-08 10:34:30 +0800

    weibotask

---
+++ src/main/java/thread/WeiboTask.java
@@ -0,0 +1,62 @@
+package thread;
+
+import entity.Account;
+import entity.User;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Connection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.AccountPool;
+import util.Config;
+import util.JsoupUtil;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by zhangxs on 2016/9/8.
+ */
+public class WeiboTask implements Runnable {
+    private static Logger log = LoggerFactory.getLogger(WeiboTask.class);
+    private User user;
+    private int page;
+    private boolean success = false;
+
+    public WeiboTask(User user, int page) {
+        this.user = user;
+        this.page = page;
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (i < 3) {
+            if (success = getWeiboList()) break;
+            i++;
+        }
+        if (!success) {
+            log.info("三次均未成功");
+        }
+    }
+
+    public boolean getWeiboList() {
+        Connection con = JsoupUtil
+                .getGetCon(Config.getValue("weiboListUrl").replaceAll("#userId#", user.getId()).replaceAll("#page#", page + ""))
+                .cookies(AccountPool.getAccount().getCookies());
+        Connection.Response rs = null;
+        try {
+            rs = con.ignoreContentType(true).execute();
+            JSONArray cards = new JSONObject(rs.body()).getJSONArray("cards");
+            JSONArray list = cards.getJSONObject(0).getJSONArray("card_group");
+            for (int i = 0; i < list.length(); i++) {
+                System.out.println(list.getJSONObject(i).getJSONObject("mblog").getString("text"));
+                //do something
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
