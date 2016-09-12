56d483ff2e63a747189d79cec25be3f8b05098a2
PorUnaCabeza <makpia@163.com>
2016-09-12 21:50:15 +0800

    diamond

--- src/main/java/util/Config.java
+++ src/main/java/util/Config.java
@@ -1,5 +1,7 @@
 package util;
 
+import org.n3r.diamond.client.Miner;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -8,7 +10,7 @@ import java.util.Properties;
  * Created by Cabeza on 2016-06-03.
  */
 public class Config {
-    private static Properties pro = new Properties();
+   /* private static Properties pro = new Properties();
     static{
         ClassLoader loader = Config.class.getClassLoader();
         InputStream in = loader.getResourceAsStream("Yeats.properties");
@@ -21,8 +23,10 @@ public class Config {
     public static String getValue(String key){
         String value = pro.getProperty(key);
         return value;
+    }*/
+
+    public static String getValue(String key) {
+        return new Miner().getMiner("yeats", "weibo").getString(key);
     }
-    public static void main(String[] args) {
-        System.out.println(getValue("url"));
-    }
+
 }
